53b00432f8f56f5816f199350bca2f04e2f55c8e
Stephen Gream <stephen.gream@gmail.com>
2026-08-19 00:21:16 +1000

    Add types to compendia script

--- utils/compendia.ts
+++ utils/compendia.ts
@@ -7,62 +7,62 @@ const PACK_SRC = "./src/packs";
 const PACK_DEST = "../packs";
 
 async function purgeDatabase(database: Datastore) {
-    await new Promise<void>((resolve, reject) => {
-        database.remove({}, {multi: true}, (error) => {
-            if(error) {
-                reject(error);
-                return;
-            }
+	await new Promise<void>((resolve, reject) => {
+		database.remove({}, { multi: true }, (error: Error) => {
+			if (error) {
+				reject(error);
+				return;
+			}
 
-            database.persistence.compactDatafile();
+			database.persistence.compactDatafile();
 
-            resolve();
-        });
-    });
+			resolve();
+		});
+	});
 }
 
 async function buildPack(folder: string) {
-    const filename = path.resolve(PACK_DEST, `${folder}.db`);
+	const filename = path.resolve(PACK_DEST, `${folder}.db`);
 
-    const database = new Datastore({
-        filename,
-        autoload: true,
-    });
+	const database = new Datastore({
+		filename,
+		autoload: true,
+	});
 
-    await purgeDatabase(database);
+	await purgeDatabase(database);
 
-    const files = await readdir(path.join(PACK_SRC, folder));
+	const files = await readdir(path.join(PACK_SRC, folder));
 
-    for (const file of files){
-        if(!file.endsWith(".yaml")) continue;
-        
-        const filename = path.join(PACK_SRC, folder, file);
-        const contents = await readFile(filename, "utf8");
-        const documents = yaml.loadAll(contents);
+	for (const file of files) {
+		if (!file.endsWith(".yaml")) continue;
 
-        await new Promise<void>((resolve, reject) => {
-            database.insert(documents, (error) => {
-                if (error) {
-                    reject(error);
-                    return;
-                }
+		const filename = path.join(PACK_SRC, folder, file);
+		const contents = await readFile(filename, "utf8");
+		const documents = yaml.loadAll(contents);
 
-                resolve();
-            });
-        });
-    }
+		await new Promise<void>((resolve, reject) => {
+			database.insert(documents, (error: Error) => {
+				if (error) {
+					reject(error);
+					return;
+				}
+
+				resolve();
+			});
+		});
+	}
 }
 
 export async function compile() {
-    await mkdir(PACK_DEST, {recursive: true});
+	await mkdir(PACK_DEST, { recursive: true });
 
-    const entries = await readdir(PACK_SRC, {
-        withFileTypes: true,
-    });
+	const entries = await readdir(PACK_SRC, {
+		withFileTypes: true,
+	});
 
-    const folders = entries
-        .filter((entry) => entry.isDirectory())
-        .map((entry) => entry.name);
+	const folders = entries
+		.filter((entry) => entry.isDirectory())
+		.map((entry) => entry.name);
 
-    await Promise.all(folders.map(buildPack));
+	await Promise.all(folders.map(buildPack));
 }

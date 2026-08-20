c12a115ea45527cdf6d6db2294e43ecb704ae50a
Stephen Gream <stephen.gream@gmail.com>
2026-08-20 23:05:29 +1000

    Start fresh with less as well

--- utils/build.ts
+++ utils/build.ts
@@ -2,6 +2,16 @@ import { bundleTypescript } from "./javascript";
 import { bundleCss } from "./css";
 import { bundlePacks } from "./compendia";
 import { cp } from "node:fs/promises";
+import { watch } from "node:fs";
+
+const WATCH_PATHS = [
+	"./src",
+	"./less",
+	"./template",
+	"./lang",
+	"./system-manifests",
+	"./src/packs",
+];
 
 async function copyStaticFiles() {
 	await cp("./system-manifests/dev.json", "./release/system.json", {
@@ -12,6 +22,7 @@ async function copyStaticFiles() {
 		recursive: true,
 		force: true,
 	});
+	await cp("./asset", "./release/asset", { recursive: true, force: true });
 }
 
 async function build() {
@@ -21,4 +32,30 @@ async function build() {
 	await copyStaticFiles();
 }
 
-await build();
+async function watchMode() {
+	await build();
+	console.log(`Watching ${WATCH_PATHS.join(", ")}...`);
+
+	let timeout: ReturnType<typeof setTimeout> | null = null;
+
+	for (const path of WATCH_PATHS) {
+		watch(path, { recursive: true }, (_event, filename) => {
+			if (filename?.includes("release/")) return;
+
+			if (timeout) clearTimeout(timeout);
+			timeout = setTimeout(() => {
+				console.log(
+					`\n[${new Date().toLocaleTimeString()}] Change detected in ${path}/${filename}`,
+				);
+				build().catch(() => {});
+			}, 150);
+		});
+	}
+}
+
+const isWatch = process.argv.includes("--watch");
+if (isWatch) {
+	await watchMode();
+} else {
+	await build();
+}
